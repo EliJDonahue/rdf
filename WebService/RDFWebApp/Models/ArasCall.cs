24648ddebef692ba5f62e4335dc8c59327df5d0d
Eli Donahue <edonahue@aras.com>
2018-09-26 17:41:46 -0400

    Track web service as part of main RDF repo, not separate repo

---
+++ WebService/RDFWebApp/Models/ArasCall.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Aras.IOM;
+
+namespace RDFWebApp.Models
+{
+    public class ArasCall
+    {
+        public static string USER = "admin";
+        public static string PWD = "innovator";
+        public static string URL = "http://araslabsvm1.eastus.cloudapp.azure.com/RDF/";
+        public static string DB = "RDF_Eli";
+
+        public string Name { get; set; }
+        public string Action { get; set; }
+        public string ItemType { get; set; }
+        public string Body { get; set; }
+        public Innovator Inn { get; set; }
+        public HttpServerConnection Conn { get; set; }
+    }
+}
